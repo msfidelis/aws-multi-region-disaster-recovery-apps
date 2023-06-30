ae70461d896db6794878ea4b268383790e9ed5e1
Matheus Fidelis <matheus@nanoshots.com.br>
2023-06-30 00:28:56 -0300

    feature(listener): initial listener and k6

---
+++ k6/load.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+
+// const base_path = "https://api.msfidelis.com.br"
+
+const base_path = "http://0.0.0.0:8080"
+
+export default function () {
+    let data = {product: "teste", amount: 223.34 };
+
+    let url = `${base_path}/sales`
+
+    let res = http.post(url, JSON.stringify(data), {
+        headers: { 'Content-Type': 'application/json' },
+      });
+      console.log(res.json().id); 
+}
