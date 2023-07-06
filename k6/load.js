4db26ef0da2be3276013636f4db3470139a7b228
Matheus Fidelis <matheus@nanoshots.com.br>
2023-07-05 23:57:09 -0300

    fix(dao): update flag

--- k6/load.js
+++ k6/load.js
@@ -1,8 +1,8 @@
 import http from 'k6/http';
 
-// const base_path = "https://api.msfidelis.com.br"
+const base_path = "https://api.msfidelis.com.br"
 
-const base_path = "http://0.0.0.0:8080"
+// const base_path = "http://0.0.0.0:8080"
 
 export default function () {
     let data = {product: "teste", amount: 223.34 };
@@ -12,5 +12,7 @@ export default function () {
     let res = http.post(url, JSON.stringify(data), {
         headers: { 'Content-Type': 'application/json' },
       });
-      console.log(res.json().id); 
+
+      console.log(res.body)
+      // console.log(res.json().id); 
 }
